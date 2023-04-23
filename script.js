37d1a73e85cc375c3644150597c0d1bb7d1e3a61
FAXOD <tommcgorrin@gmail.com>
2023-04-23 20:12:55 +0100

    remove code causing validation to skip

--- script.js
+++ script.js
@@ -5,27 +5,9 @@ myform.addEventListener('submit', validateForm);
 
 function validateForm(e) {
     const form = e.target;
-    const password = e.target.elements.getElementById('password');
-    const confirmPassword = e.target.elements.getElementById('confirm-password');
 
     if (form.checkValidity()) {
-        if (password.textContent === confirmPassword.textContent) {
-            
-        } else {
-            e.preventDefault();
-            Array.from(form.elements).forEach(i => {
-                i.classList.add('valid');
-                i.classList.remove('invalid');
-                if (i.classList.contains('password') | i.classList.contains('confirm-password')) {
-                    i.classList.add('invalid');
-                    i.classList.remove('valid');
-                }
-                if (i.classList.contains('btn')) {
-                    i.classList.remove('valid');
-                    i.classList.remove('invalid');
-                }
-            })
-        }
+        
     } else {
         e.preventDefault();
         Array.from(form.elements).forEach(i => {
